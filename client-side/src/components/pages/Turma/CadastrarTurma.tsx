50dd40c2fd34aae8410547c36cc54eb3802aff41
luckas-ferreira <luckas0ferreira321@gmail.com>
2024-08-25 16:59:37 -0300

    [ADS-141]:feat-Adicionado loading nos butões.

--- client-side/src/components/pages/Turma/CadastrarTurma.tsx
+++ client-side/src/components/pages/Turma/CadastrarTurma.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
@@ -27,8 +27,14 @@ function Form() {
     const tipoAulaValues: any = watch("tipoAula");
 
     const onSubmit = (data: FormData) => {
+        setClick(true);
         console.log(data);
+        setTimeout(() => {
+            setClick(false);
+        }, 2000);
     };
+
+    const [click, setClick] = useState(false);
 
     const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { value, checked } = event.target;
@@ -124,7 +130,13 @@ function Form() {
 
             {/* Submit Button */}
             <div className="col-span-12 mb-5 sm:mb-0 flex justify-end">
-                <Button text="Cadastrar" type="submit" />
+                {click ? (
+                    <svg width="30" height="30" fill="currentColor" className="mr-2 text-button_blue animate-spin" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z"></path>
+                    </svg>
+                ) : (
+                    <Button text="Cadastrar" type="submit" />
+                )}
             </div>
         </form>
     );
--- client-side/src/components/utils/Button.tsx
+++ client-side/src/components/utils/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     return (
       <button
         type={type}
-        className="text-white bg-button_blue hover:bg-button_blue_hover focus:ring-4 focus:ring-button_blue_active font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+        className="text-white bg-button_blue hover:bg-button_blue_hover focus:ring-4 focus:ring-button_blue_active font-medium rounded-lg text-sm px-5 py-2.5"
         {...rest}
       >
         {to && type === 'button' ? (

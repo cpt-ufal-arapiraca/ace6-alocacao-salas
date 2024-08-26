a1ec9efa3352452838b3f46aa5d5be50866e5f76
wellingtonsuper17 <wellgt555@gmail.com>
2024-08-26 12:29:19 -0300

    div centralizada feature/ADS-127/notFound

--- client-side/src/components/error/NotFound.tsx
+++ client-side/src/components/error/NotFound.tsx
@@ -3,17 +3,17 @@ import Img from "./Error-404.png";
 
 function NotFound() {
     return (
-        <div className="h-full flex items-center">
-            <img src={Img} alt="error" />
+        <div className="h-full flex items-center ">
+        <img src={Img} alt="error" /> 
+            
 
-
-            <div className="h-full text-center">
-                <h1 className="text-5xl"><b>404 - Página não encontrada!</b></h1>
-                <p className="text-2xl"> Desculpe, a página que você está procurando não <br />
-                    foi encontrada ou o caminho está incorreto.</p>
-                <Button to="alocsalas" text="Tela inicial">
-                </Button>
-            </div>
+        <div className="text-center ">
+        <h1 className="text-5xl text-center mb-4" ><b>404 - Página não encontrada!</b></h1>
+        <p className="text-2xl text-center mb-4"> Desculpe, a página que você está procurando não
+         foi encontrada ou o caminho está incorreto.</p>
+        <div className=""><Button to="inicio" text="Tela inicial">
+        </Button></div>
+        </div>
         </div>
 
 

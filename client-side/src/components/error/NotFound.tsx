392350e957d21ee29fb12057da2660b0ac007ebf
wellingtonsuper17 <wellgt555@gmail.com>
2024-08-26 13:12:13 -0300

    ajuste de texto feature/ADS-127

--- client-side/src/components/error/NotFound.tsx
+++ client-side/src/components/error/NotFound.tsx
@@ -9,7 +9,7 @@ function NotFound() {
 
         <div className="text-center ">
         <h1 className="text-5xl text-center mb-4" ><b>404 - Página não encontrada!</b></h1>
-        <p className="text-2xl text-center mb-4"> Desculpe, a página que você está procurando não
+        <p className="text-balance text-2xl text-center mb-4">Desculpe, a página que você está procurando não
          foi encontrada ou o caminho está incorreto.</p>
         <div className=""><Button to="inicio" text="Tela inicial">
         </Button></div>

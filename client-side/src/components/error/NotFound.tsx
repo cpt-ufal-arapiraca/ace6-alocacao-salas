5a111b68b7c529234425e9b74375fc1f9d3cd17a
wellingtonsuper17 <wellgt555@gmail.com>
2024-08-27 10:11:29 -0300

    imagem centralizada feature/ADS-127

--- client-side/src/components/error/NotFound.tsx
+++ client-side/src/components/error/NotFound.tsx
@@ -3,11 +3,11 @@ import Img from "./Error-404.png";
 
 function NotFound() {
     return (
-        <div className="grid grid-cols-12 ">
-            <div className="col-span-4">
-                <img src={Img}  alt="error" />
+        <div className="grid grid-cols-12">
+            <div className="col-span-3 hidden sm:block">
+                <img className="object-fill h-screen" src={Img}  alt="error" />
             </div>
-            <div className="col-span-8 flex justify-center items-center flex-col">
+            <div className="sm:col-span-9 col-span-12 flex justify-center items-center flex-col">
                 <div className="text-center">
                     <h1 className="text-5xl  mb-4" ><b>404 - Página não encontrada!</b></h1>
                     <p className="text-balance text-2xl mb-4">Desculpe, a página que você está procurando não

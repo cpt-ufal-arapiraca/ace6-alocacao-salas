f88622289085cad8aa6bdf020666b318db41661a
wellingtonsuper17 <wellgt555@gmail.com>
2024-08-27 14:00:26 -0300

    [ADS-127]: feat-tela notFound finalizada

--- client-side/src/components/error/NotFound.tsx
+++ client-side/src/components/error/NotFound.tsx
@@ -3,23 +3,20 @@ import Img from "./Error-404.png";
 
 function NotFound() {
     return (
-        <div className="grid grid-cols-12">
+        <div className="grid grid-cols-12 h-screen">
             <div className="col-span-3 hidden sm:block">
                 <img className="object-fill h-screen" src={Img}  alt="error" />
             </div>
             <div className="sm:col-span-9 col-span-12 flex justify-center items-center flex-col">
                 <div className="text-center">
-                    <h1 className="text-5xl  mb-4" ><b>404 - Página não encontrada!</b></h1>
+                    <h1 className="sm:text-5xl text-3xl  mb-4" ><b>404 - Página não encontrada!</b></h1>
                     <p className="text-balance text-2xl mb-4">Desculpe, a página que você está procurando não
                     foi encontrada ou o caminho está incorreto.</p>
 
                 </div>
-                <div className=""><Button to="inicio" text="Tela inicial"></Button></div>
+                <div className="mt-2"><Button to="inicio" text="Tela inicial"></Button></div>
             </div>
         </div>
-
-
-
     )
 }
 
